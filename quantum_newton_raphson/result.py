f842f7763cbd49b2f8a65f584a55263cdaa1f443
Nicolas Renaud <nicolas.gm.renaud@gmail.com>
2024-01-17 15:19:39 +0100

    reformat

--- quantum_newton_raphson/result.py
+++ quantum_newton_raphson/result.py
@@ -1,31 +1,35 @@
 from dataclasses import dataclass
-import numpy as np 
-from typing import List 
-from scipy.sparse.linalg  import SuperLU
+import numpy as np
+from typing import List
+from scipy.sparse.linalg import SuperLU
+
 
 @dataclass
 class NewtonRaphsonResult:
     solution: np.ndarray
-    n_iter: int 
+    n_iter: int
     diff: float
     converged: bool
     linear_solver_results: List
+
 
 @dataclass
 class SPLUResult:
     solution: np.ndarray
     splu: SuperLU
 
-@dataclass 
+
+@dataclass
 class VQLSResult:
     solution: np.ndarray
-    # n_iter: int 
-    # error: float 
+    # n_iter: int
+    # error: float
     # cost_function: np.ndarray
 
-@dataclass 
+
+@dataclass
 class QUBOResult:
     solution: np.ndarray
-    # n_iter: int 
-    # error: float 
+    # n_iter: int
+    # error: float
     # cost_function: np.ndarray
--- quantum_newton_raphson/splu_solve.py
+++ quantum_newton_raphson/splu_solve.py
@@ -1,7 +1,8 @@
-import numpy as np 
+import numpy as np
 from typing import Dict
 from scipy.sparse import sparray, triu
 from .result import SPLUResult
+
 
 def get_ordering(A: sparray, reorder_method: str, options: Dict):
     """get the reordering
@@ -12,13 +13,14 @@ def get_ordering(A: sparray, reorder_method: str, options: Dict):
         options (Dict): options of the reodering method
     """
 
-    if reorder_method == 'max_edge':
+    if reorder_method == "max_edge":
         idx = get_max_edge_ordering(A, **options)
-    elif reorder_method == 'quantum':
+    elif reorder_method == "quantum":
         idx = get_quantum_ordering(A, **options)
     else:
-        raise ValueError('Ordering method not recognized')
+        raise ValueError("Ordering method not recognized")
     return idx
+
 
 def get_max_edge_ordering(A: sparray):
     """Get ordering of the matrix
@@ -28,6 +30,7 @@ def get_max_edge_ordering(A: sparray):
     """
     return np.argsort(triu(A, k=1).sum(1))
 
+
 def get_quantum_ordering(A: sparray, options: Dict = {}):
     """Get the ordering of the matrix element following the quantum approach
 
@@ -35,7 +38,10 @@ def get_quantum_ordering(A: sparray, options: Dict = {}):
         A (sparray): inout matrix
         options (Dict, optional): options of the quantum routine Defaults to {}.
     """
-    raise NotImplementedError("Quantum routine for matrix reordeing not implemented yet")
+    raise NotImplementedError(
+        "Quantum routine for matrix reordeing not implemented yet"
+    )
+
 
 def splu_solve(A: sparray, b: sparray, options: Dict = {}):
     """Solve the linear system by reordering the system of eq.
@@ -45,9 +51,9 @@ def splu_solve(A: sparray, b: sparray, options: Dict = {}):
         b (sparray): input rhs
         options (dict, optional): _description_. Defaults to {}.
     """
-    
+
     # get order
-    reorder_method = options.pop('reorder') if 'reoder' in options else 'max_edge'
+    reorder_method = options.pop("reorder") if "reoder" in options else "max_edge"
     order = get_ordering(A, reorder_method)
 
     # reorder matrix and rhs
@@ -55,10 +61,9 @@ def splu_solve(A: sparray, b: sparray, options: Dict = {}):
     b = b[order]
 
     # solve
-    splu = splu(A, permc_spec='NATURAL')
+    splu = splu(A, permc_spec="NATURAL")
     x = splu.solve(b)
 
     # reorder solution
     x = x[np.argsort(order)]
     return SPLUResult(x, splu)
-
