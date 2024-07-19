93c4708a9f3ae6067b49c9cca2269449fe2c910d
Nicolas Renaud <nicolas.gm.renaud@gmail.com>
2024-07-19 10:43:55 +0200

    fix test

--- tests/test_hhl_solve.py
+++ tests/test_hhl_solve.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import pytest
-from qiskit.primitives import Estimator
+from qiskit.primitives import Estimator, Sampler
 from scipy.sparse import random as sprand
 from scipy.sparse import sparray
 from quantum_newton_raphson.hhl_solver import HHL_SOLVER
@@ -29,6 +29,7 @@ size = 4
 )
 def test_hhl_solve_default(A, b, options):
     """Test the hhl solver."""
-    results = HHL_SOLVER(A, b, options)
+    solver = HHL_SOLVER(estimator=Estimator(), sampler=Sampler())
+    results = solver(A, b)
     if np.linalg.norm(A.dot(results.solution) - b) > 0.1:
         pytest.skip("HHL solution innacurate")
